32f9ba8609ae37752528a55509c30e56a1f55c81
Benjamin den Boer <benjamindenboer@hotmail.com>
2015-01-06 10:11:53 +0100

    iOS Bookmark Behaviour (Ex.Page)

--- static/js/preview.coffee
+++ static/js/preview.coffee
@@ -1,24 +1,37 @@
-loadExample = (exampleName, exampleNameNoHash) ->
+loadExample = (exampleName) ->
 	ga("send", "pageview", "http://framerjs.com/examples/#{exampleName}") if ga?
 
 	$("#code").attr "src", "code.html?name=#{exampleName}"
 	$("#example").attr "src", "http://projects.framerjs.com/static/examples/#{exampleName}"
-	$(".btn-dl").attr "href", "http://projects.framerjs.com/static/examples/#{exampleNameNoHash}.zip"
-	$(".btn-open").attr "href", "http://framer.link/projects.framerjs.com/static/examples/#{exampleNameNoHash}.zip"
+	$(".btn-dl").attr "href", "http://projects.framerjs.com/static/examples/#{exampleName}.zip"
+	$(".btn-open").attr "href", "http://framer.link/projects.framerjs.com/static/examples/#{exampleName}.zip"
 
 $(document).ready ->
 
-	exampleName = window.location.hash[1..]
-	exampleNameNoHash = window.location.hash[1..]
+	parts = window.location.href.split("#")
 
-	# For document title
-	if exampleName.indexOf("#code") > -1
-		document.title = window.location.hash[1..-6]
-		# For download link
-		exampleNameNoHash = window.location.hash[1..-6]
+	if parts.length == 2
+		exampleName = parts[1]
+		exampleShowCode = false
 
-	loadExample exampleName, exampleNameNoHash
+	else if parts.length == 3
+		exampleName = parts[1]
+		exampleShowCode = true
+
+	else
+		exampleName = ""
+		exampleShowCode = false
+
+	# exampleName = window.location.hash[1..]
 	document.title = exampleName
+
+	# # For document title
+	# if exampleName.indexOf("#code") > -1
+	# 	# For download link
+	# 	exampleNameNoHash = window.location.hash[1..-6]
+	# 	document.title = exampleNameNoHash
+
+	loadExample(exampleName)
 
 	$(".btn-close").hide()	
 
@@ -34,7 +47,20 @@ $(document).ready ->
 		$(".btn-close").hide()	
 		$(".btn-code").show()	
 
-	if exampleName.indexOf("#code") > -1
+	if exampleShowCode is true
 		$("#code").addClass "with-code"
 		$("#example").addClass "with-code"	
 		$(".btn-code").hide()		
+
+	# If bookmarked
+	if (window.navigator.standalone)
+		window.location.href = "http://projects.framerjs.com/static/examples/#{exampleName}"
+
+	# 	# if window.location.href.indexOf("%23code") > -1 
+	# 	# 	window.location = "http://projects.framerjs.com/static/examples/#{exampleNameNoHash}"
+
+mq = window.matchMedia("(max-width:800px)")
+
+if (mq.matches)
+	window.location.hash = '#' + window.location.hash.split('#')[1]
+	console.log("MATCH")
--- static/js/preview.js
+++ static/js/preview.js
@@ -1,26 +1,31 @@
 (function() {
-  var loadExample;
+  var loadExample, mq;
 
-  loadExample = function(exampleName, exampleNameNoHash) {
+  loadExample = function(exampleName) {
     if (typeof ga !== "undefined" && ga !== null) {
       ga("send", "pageview", "http://framerjs.com/examples/" + exampleName);
     }
     $("#code").attr("src", "code.html?name=" + exampleName);
     $("#example").attr("src", "http://projects.framerjs.com/static/examples/" + exampleName);
-    $(".btn-dl").attr("href", "http://projects.framerjs.com/static/examples/" + exampleNameNoHash + ".zip");
-    return $(".btn-open").attr("href", "http://framer.link/projects.framerjs.com/static/examples/" + exampleNameNoHash + ".zip");
+    $(".btn-dl").attr("href", "http://projects.framerjs.com/static/examples/" + exampleName + ".zip");
+    return $(".btn-open").attr("href", "http://framer.link/projects.framerjs.com/static/examples/" + exampleName + ".zip");
   };
 
   $(document).ready(function() {
-    var exampleName, exampleNameNoHash;
-    exampleName = window.location.hash.slice(1);
-    exampleNameNoHash = window.location.hash.slice(1);
-    if (exampleName.indexOf("#code") > -1) {
-      document.title = window.location.hash.slice(1, -5);
-      exampleNameNoHash = window.location.hash.slice(1, -5);
+    var exampleName, exampleShowCode, parts;
+    parts = window.location.href.split("#");
+    if (parts.length === 2) {
+      exampleName = parts[1];
+      exampleShowCode = false;
+    } else if (parts.length === 3) {
+      exampleName = parts[1];
+      exampleShowCode = true;
+    } else {
+      exampleName = "";
+      exampleShowCode = false;
     }
-    loadExample(exampleName, exampleNameNoHash);
     document.title = exampleName;
+    loadExample(exampleName);
     $(".btn-close").hide();
     $(".btn-code").click(function() {
       $("#example").toggleClass("with-code");
@@ -34,11 +39,21 @@
       $(".btn-close").hide();
       return $(".btn-code").show();
     });
-    if (exampleName.indexOf("#code") > -1) {
+    if (exampleShowCode === true) {
       $("#code").addClass("with-code");
       $("#example").addClass("with-code");
-      return $(".btn-code").hide();
+      $(".btn-code").hide();
+    }
+    if (window.navigator.standalone) {
+      return window.location.href = "http://projects.framerjs.com/static/examples/" + exampleName;
     }
   });
+
+  mq = window.matchMedia("(max-width:800px)");
+
+  if (mq.matches) {
+    window.location.hash = '#' + window.location.hash.split('#')[1];
+    console.log("MATCH");
+  }
 
 }).call(this);
